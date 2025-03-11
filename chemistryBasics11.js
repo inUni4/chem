84553de3771bad5f532488cdc5228c741f4ce30b
inUni4 <166997704+inUni4@users.noreply.github.com>
2025-03-11 14:51:01 +0200

    Add files via upload

--- chemistryBasics11.js
+++ chemistryBasics11.js
@@ -1,5 +1,5 @@
-const chemistryA = ["The study of matter.","Anything that occupies space and has mass.", "The measure of the amount of matter that an object contains.", "A tentative explanation or prediction of experimental observations.", "A concise verbal or mathematical statement of a relation that is always the same under any conditions.", "A well tested, unifying principle that explains a body of facts and the laws based on them.", "Something, an event, phenomena that is noticed or observed.", "An organized procedure used to test a hypothesis."];
-const chemistryQ = ["What is chemistry?", "What is matter?", "What is mass?", "What is an hypothesis?", "What is a law?", "What is a theory?", "What is an observation?", "What is an experiment?"];
+const chemistryA = ["You cannot see the different parts of the mixture, it is even throughout.","You see the different parts of the mixture easily and the mixture is not even.", "A solution is a homogenous mixture that combines a solute and a solvent.", "The solid part that gets dissolved.", "The liquid part that dissolves.", "The measurement of how easily a solute will dissolve in a certain solvent.", "A solution with only a small amount of solute.", "A solution with a large amount of solute."];
+const chemistryQ = ["What is a homogenous mixture?", "What is a heterogenous mixture?", "What is a solution?", "What is a solute?", "What is a solvent?", "What is solubility?", "What is a dilute solution?", "What is a concentrated solution?"];
 const chemWrapper = document.querySelector(".chem-wrapper");
 const chemQuestions = document.querySelector(".chem-questions");
 const chemAnswers = document.querySelector(".chem-answers");
@@ -29,39 +29,39 @@ chemSubmit.addEventListener("click", () => {
     chemVerify.classList.remove("chem-verify-bad");
     chemVerify.classList.add("chem-verify-good");
     chemVerify.textContent = "You are correct!";
-    chemQuestions.textContent = chemistryQ[3];
     chemAnswers.value = "";
+    chemQuestions.textContent = chemistryQ[3];
   }  else if(chemQuestions.textContent == chemistryQ[3] && chemAnswers.value == chemistryA[3]) {
     chemVerify.classList.remove("hide");
     chemVerify.classList.remove("chem-verify-bad");
     chemVerify.classList.add("chem-verify-good");
     chemVerify.textContent = "You are correct!";
+    chemAnswers.value = "";
     chemQuestions.textContent = chemistryQ[4];
-    chemAnswers.value = "";
-  } else if(chemQuestions.textContent == chemistryQ[4] && chemAnswers.value == chemistryA[4]) {
+  }  else if(chemQuestions.textContent == chemistryQ[4] && chemAnswers.value == chemistryA[4]) {
     chemVerify.classList.remove("hide");
     chemVerify.classList.remove("chem-verify-bad");
     chemVerify.classList.add("chem-verify-good");
     chemVerify.textContent = "You are correct!";
+    chemAnswers.value = "";
     chemQuestions.textContent = chemistryQ[5];
-    chemAnswers.value = "";
-  } else if(chemQuestions.textContent == chemistryQ[5] && chemAnswers.value == chemistryA[5]) {
+  }  else if(chemQuestions.textContent == chemistryQ[5] && chemAnswers.value == chemistryA[5]) {
     chemVerify.classList.remove("hide");
     chemVerify.classList.remove("chem-verify-bad");
     chemVerify.classList.add("chem-verify-good");
     chemVerify.textContent = "You are correct!";
+    chemAnswers.value = "";
     chemQuestions.textContent = chemistryQ[6];
-    chemAnswers.value = "";
-  } else if(chemQuestions.textContent == chemistryQ[6] && chemAnswers.value == chemistryA[6]) {
+  }  else if(chemQuestions.textContent == chemistryQ[6] && chemAnswers.value == chemistryA[6]) {
     chemVerify.classList.remove("hide");
     chemVerify.classList.remove("chem-verify-bad");
     chemVerify.classList.add("chem-verify-good");
     chemVerify.textContent = "You are correct!";
-    chemQuestions.textContent = chemistryQ[7];
     chemAnswers.value = "";
+    chemQuestions.textContent = chemistryQ[7];
   } else if(chemQuestions.textContent == chemistryQ[7] && chemAnswers.value == chemistryA[7]) {
     chemVerify.classList.add("hide");
-    chemQuestions.innerHTML = "Congratulations, you can move on to the next <a href='https://inuni4.github.io/chem/chemistryBasics1.html'>level</a>";
+    chemQuestions.innerHTML = "Congratulations, you are done for the day.";
     chemAnswers.classList.add("hide");
     chemSubmit.classList.add("hide");
     chemAnswers.value = "";
